71efc285237aa2401a9e8d1dc19bfa2cc406b7f7
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-06 22:48:39 +0530

    hh

---
+++ group anagrams.py
@@ -0,0 +1,34 @@
+def groupana(ans):
+    map={}
+    for i in ans:
+        s="".join(sorted(i))
+        if s in map:
+            map[s].append(i)
+        else:
+            map[s]=[i]
+
+
+    print(map)
+    res=[]
+    for i in map.values():
+        res.append(i)
+
+    print(res)
+    return 
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+aaa=["eat","tea","tan","ate","nat","bat"]
+groupana(aaa)
