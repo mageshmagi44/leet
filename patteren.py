0a6d0a37f6ce2131124ac5d5067637442b04b469
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-14 20:03:22 +0530

    sorting algos

--- patteren.py
+++ patteren.py
@@ -35,7 +35,63 @@ def insertion (nums):
         nums[pos]=curr
 
 
-print(nums)
-insertion(nums)
-print(nums)
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+def mergesort(lists):
+    if len(lists)>1:
+        mid=len(lists)//2
+        left=lists[:mid]
+        right=lists[mid:]
+        mergesort(left)
+        mergesort(right)
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                lists[k]=left[i]
+                i+=1
+                k+=1
+            else:
+                lists[k]=right[j]
+                j+=1
+                k+=1
+        while i < len(left):
+            lists[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            lists[k]=right[j]
+            j+=1
+            k+=1
+
+
+
+            
+def bubble_sortrec(arr):
+    n = len(arr)
+    count=0
+    for i in range(n-1):
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            count+=1
+    if count==0:
+        return arr
+    else:
+        bubble_sortrec(arr)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+
+print(arr)
+bubble_sortrec(arr)
+print(arr)
